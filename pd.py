68d0f5635a980118e5db8b997e254387e21292df
BuildTools <unconfigured@null.spigotmc.org>
2020-01-08 15:00:46 +1100

    Improve readability and PEP 8 compliance

--- pd.py
+++ pd.py
@@ -1,5 +1,22 @@
-import sys, os
-os.system('pakku -Qqe > host_pkgs.txt')
-ref, host, command = [line.rstrip('\n') for line in open('ref_pkgs.txt', 'r')], [line.rstrip('\n') for line in open('host_pkgs.txt', 'r')], "pakku -S"
-for x in (r for r in ref if r not in host): command += " " + x
-if input('\n' + command + '\n\nAre you sure you would like to run the above command?\nYou will be installing ' + str(len(ref) - len(host)) + ' packages. [y/n]: ').lower() == 'y': os.system(command)
+import os
+import sys
+
+os.system("pakku -Qqe > host_pkgs.txt")
+ref, host, command = (
+    [line.rstrip("\n") for line in open("ref_pkgs.txt", "r")],
+    [line.rstrip("\n") for line in open("host_pkgs.txt", "r")],
+    "pakku -S",
+)
+for x in (r for r in ref if r not in host):
+    command += " " + x
+if (
+    input(
+        "\n"
+        + command
+        + "\n\nAre you sure you would like to run the above command?\nYou will be installing "
+        + str(len(ref) - len(host))
+        + " packages. [y/n]: "
+    ).lower()
+    == "y"
+):
+    os.system(command)
